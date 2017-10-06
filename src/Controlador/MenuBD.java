317f7d81018dc9b66ad8851cc2da9b70d99104fe
kathe1102 <32551836+kathe1102@users.noreply.github.com>
2017-10-06 08:29:00 -0500

    Initial commit

---
+++ src/Controlador/MenuBD.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+import Vista.Inicio;
+
+/**
+ *
+ * @author SENA
+ */
+public class MenuBD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Inicio ca=new Inicio();
+        ca.setVisible(true);
+       ca.setLocationRelativeTo(null);
+       ca.setSize(300,360);
+    }
+    
+}
